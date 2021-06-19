6a9fc5006da9f9da403f582ed68fa8b8ad150675
Pavlo Kerestey <pavlo.kerestey@thoughtworks.com>
2021-06-19 08:19:43 +0200

    First attempt with gota

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.16
 
 require (
 	cloud.google.com/go v0.84.0 // indirect
+	github.com/go-gota/gota v0.10.1 // indirect
 	github.com/google/go-cmp v0.5.6 // indirect
 	github.com/guptarohit/asciigraph v0.5.2 // indirect
 	github.com/mattn/go-runewidth v0.0.13 // indirect
@@ -11,4 +12,5 @@ require (
 	github.com/rocketlaunchr/dataframe-go v0.0.0-20210422123815-aaa951b82e1b // indirect
 	golang.org/x/exp v0.0.0-20210617012816-0dc655b38382 // indirect
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	gonum.org/v1/gonum v0.9.2 // indirect
 )
--- main.go
+++ main.go
@@ -8,17 +8,18 @@ import (
   "log"
   "strconv"
   "time"
-  context "context"
 
-  dataframe "github.com/rocketlaunchr/dataframe-go"
+  dataframe "github.com/go-gota/gota/dataframe"
+  series "github.com/go-gota/gota/series"
   civil "cloud.google.com/go/civil"
 )
 
-func parseInt64DefaultZero(s string) int64 {
-  res, err := strconv.ParseInt(s, 10, 64)
+func parseIntDefaultZero(s string) int {
+  res, err := strconv.Atoi(s)
   if err != nil {
     return 0
-  } else { return res }
+  } 
+  return res
 }
 
 func parseDateNum(s string) civil.Date {
@@ -31,7 +32,7 @@ func parseDateNum(s string) civil.Date {
   return civil.Date{year, time.Month(month), day}
 }
 
-func readIncidencePoland() *dataframe.DataFrame {
+func readIncidencePoland() dataframe.DataFrame {
   f, err := os.Open("./data/data-pl.csv")
   if err != nil {
     log.Fatal(err)
@@ -41,11 +42,10 @@ func readIncidencePoland() *dataframe.DataFrame {
   reader.Comma = ';'
   reader.FieldsPerRecord = -1
 
-  polandDateNum := dataframe.NewSeriesGeneric("DateNum", civil.Date{}, &dataframe.SeriesInit{})
-  polandCounty := dataframe.NewSeriesString("County", &dataframe.SeriesInit{})
-  polandCity := dataframe.NewSeriesString("City", &dataframe.SeriesInit{})
-  polandCases := dataframe.NewSeriesInt64("Cases", &dataframe.SeriesInit{})
-  df := dataframe.NewDataFrame(polandDateNum, polandCounty, polandCity, polandCases)
+  dateNum := series.New([]string{}, series.String, "DateNum")
+  county := series.New([]string{}, series.String, "County")
+  city := series.New([]string{}, series.String, "City")
+  cases := series.New([]int{}, series.Int, "Cases")
 
   for {
         record, err := reader.Read()
@@ -58,13 +58,16 @@ func readIncidencePoland() *dataframe.DataFrame {
             log.Fatal(err)
         }
 
-        df.Append(nil, parseDateNum(record[0]), record[1], record[2], parseInt64DefaultZero(record[3]))
+        dateNum.Append(record[0])
+        county.Append(record[1])
+        city.Append(record[2])
+        cases.Append(parseIntDefaultZero(record[3]))
   }
 
-  return df
+  return dataframe.New(dateNum, county, city, cases)
 }
 
-func readIncidenceGermany() *dataframe.DataFrame {
+func readIncidenceGermany() dataframe.DataFrame {
   f, err := os.Open("./data/data-de.csv")
   if err != nil {
     log.Fatal(err)
@@ -73,12 +76,11 @@ func readIncidenceGermany() *dataframe.DataFrame {
   reader := csv.NewReader(f)
   reader.FieldsPerRecord = -1
 
-  date := dataframe.NewSeriesString("Date", &dataframe.SeriesInit{})
-  county := dataframe.NewSeriesString("County", &dataframe.SeriesInit{})
-  city := dataframe.NewSeriesString("City", &dataframe.SeriesInit{})
-  cases := dataframe.NewSeriesInt64("Cases", &dataframe.SeriesInit{})
-  caseFreshness := dataframe.NewSeriesInt64("CaseFreshness", &dataframe.SeriesInit{})  
-  df := dataframe.NewDataFrame(date, county, city, cases, caseFreshness)
+  date := series.New([]string{}, series.String, "Date")
+  county := series.New([]string{}, series.String, "County")
+  city := series.New([]string{}, series.String, "City")
+  cases := series.New([]int{}, series.Int, "Cases")
+  caseFreshness := series.New([]int{}, series.Int, "CaseFreshness")
 
   // skip header
   reader.Read()
@@ -94,44 +96,47 @@ func readIncidenceGermany() *dataframe.DataFrame {
             log.Fatal(err)
         }
 
-        df.Append(nil, record[8], record[2], record[3], parseInt64DefaultZero(record[6]), parseInt64DefaultZero(record[11]))
+        date.Append(record[8])
+        county.Append(record[2])
+        city.Append(record[3])
+        cases.Append(parseIntDefaultZero(record[6]))
+        caseFreshness.Append(parseIntDefaultZero(record[11]))
   }
 
-  return df
+  return dataframe.New(date, county, city, cases, caseFreshness)
 }
 
 func main() {
   poland := readIncidencePoland()
   germany := readIncidenceGermany()
 
-  filterBerlin := dataframe.FilterDataFrameFn(func(vals map[interface{}]interface{}, row, nRows int) (dataframe.FilterAction, error) {
-    if vals["County"] == "Berlin" {
-      return dataframe.KEEP, nil
-    }
-    return dataframe.DROP, nil
-  })
+  berlin := germany.Filter(
+    dataframe.F{
+      Colname:    "County",
+      Comparator: series.Eq,
+      Comparando: "Berlin",
+    },
+  )
 
-  filterZachodniopomorskie := dataframe.FilterDataFrameFn(func(vals map[interface{}]interface{}, row, nRows int) (dataframe.FilterAction, error) {
-    if vals["County"] == "zachodniopomorskie" {
-      return dataframe.KEEP, nil
-    }
-    return dataframe.DROP, nil
-  })
+  zachodniopomorskie := poland.Filter(
+    dataframe.F{
+      Colname:    "County",
+      Comparator: series.Eq,
+      Comparando: "zachodniopomorskie",
+    },
+  )
 
-  filterSzczecin := dataframe.FilterDataFrameFn(func(vals map[interface{}]interface{}, row, nRows int) (dataframe.FilterAction, error) {
-    if vals["City"] == "Szczecin" {
-      return dataframe.KEEP, nil
-    }
-    return dataframe.DROP, nil
-  })
-  
-  berlin, _ := dataframe.Filter(context.Background(), germany, filterBerlin)
-  zachodniopomorskie, _ := dataframe.Filter(context.Background(), poland, filterZachodniopomorskie)
-  szczecin, _ := dataframe.Filter(context.Background(), zachodniopomorskie.(*dataframe.DataFrame), filterSzczecin)
+  szczecin := zachodniopomorskie.Filter(
+    dataframe.F{
+      Colname:    "City",
+      Comparator: series.Eq,
+      Comparando: "Szczecin",
+    },
+  )
 
-  end := 10
-  opts := dataframe.TableOptions{R: &dataframe.Range{End: &end}}
-  fmt.Print(berlin.(*dataframe.DataFrame).Table(opts))
-  fmt.Print(zachodniopomorskie.(*dataframe.DataFrame).Table(opts))
-  fmt.Print(szczecin.(*dataframe.DataFrame).Table(opts))
+  fmt.Println(poland)
+  fmt.Println(germany)
+  fmt.Println(berlin)
+  fmt.Println(zachodniopomorskie)
+  fmt.Println(szczecin)
 }
