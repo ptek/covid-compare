ac16d3437807667d8757b1f9df021b9f9fd22294
Pavlo Kerestey <pavlo.kerestey@thoughtworks.com>
2021-06-23 13:26:17 +0200

    Fix get-data to be less verbose and use alpine xargs parameters

--- scripts/get-data.sh
+++ scripts/get-data.sh
@@ -6,7 +6,7 @@ set -o pipefail
 get-de() {
   result="${PROJECT_ROOT}/data/data-de.csv"
   mkdir -p "$(dirname $result)"
-  wget -O "$result" "https://arcgis.com/sharing/rest/content/items/f10774f1c63e40168479a1feb6c7ca74/data"
+  wget -nv -O "$result" "https://arcgis.com/sharing/rest/content/items/f10774f1c63e40168479a1feb6c7ca74/data"
 }
 
 get-pl() {
@@ -15,14 +15,14 @@ get-pl() {
   archive="${PROJECT_ROOT}/data/data-pl.zip"
   archive_extract=$(mktemp -d)
 
-  wget -O "$archive" "https://arcgis.com/sharing/rest/content/items/e16df1fa98c2452783ec10b0aea4b341/data"
+  wget -nv -O "$archive" "https://arcgis.com/sharing/rest/content/items/e16df1fa98c2452783ec10b0aea4b341/data"
   unzip -q -o "$archive" -d "$archive_extract"
 
-  find "$archive_extract" -name "*_rap_rcb_pow_eksport.csv" -print0 | xargs -L1 -0 -I{} enconv -L pl -x UTF-8 {}
-  find "$archive_extract" -name "*_rap_rcb_pow_eksport.csv" -print0 | xargs -L1 -0 -I{} bash -c "dt=\$(basename {} | head -c 8); sed -i -e \"s/^/\$dt;/\" {}"
+  find "$archive_extract" -name "*_rap_rcb_pow_eksport.csv" -print0 | xargs -n1 -0 -I{} enconv -L pl -x UTF-8 {}
+  find "$archive_extract" -name "*_rap_rcb_pow_eksport.csv" -print0 | xargs -n1 -0 -I{} bash -c "dt=\$(basename {} | head -c 8); sed -i -e \"s/^/\$dt;/\" {}"
 
   echo -n > $result
-  find "$archive_extract" -name "*_rap_rcb_pow_eksport.csv" -print0 | xargs -L1 -0 -I{} bash -c "tail +2 {} >>$result"
+  find "$archive_extract" -name "*_rap_rcb_pow_eksport.csv" -print0 | xargs -n1 -0 -I{} bash -c "tail +2 {} >>$result"
 
   rm -rf "$archive_extract"
 }
